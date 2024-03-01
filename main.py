65670aa7729d00c4f71eb966bedcea7b57780117
hieldsee <96329931+hieldsee@users.noreply.github.com>
2024-03-01 10:28:47 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import keyboard
+from map import *
+
+mapDet = {}
+
+map = gameMap(5, 20, 4, 2, mapDet)
+
+while True:
+    map.draw()
+    print('''\n
+w - up, a - left, s - down, d - right.
+
+x - attack, i - inventory, q - quit.
+          ''')
+    map.move(input("\nAction: ").lower())
